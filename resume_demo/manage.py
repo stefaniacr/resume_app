0a4b49955273f4c304157d64dacd31fed50e4921
stefaniacr <steficr@gmail.com>
2021-12-27 10:21:10 +0000

    Update manage.py

--- resume_demo/manage.py
+++ resume_demo/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
+    """Run administrative tasks. hohora hora hora """
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resume_demo.settings')
     try:
         from django.core.management import execute_from_command_line
